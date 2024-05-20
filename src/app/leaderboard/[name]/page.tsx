a396a741e9cf57144892f55768823b46e2c35309
Khisab Kurniawan <khisabkurniawan@outlook.com>
2024-05-20 21:34:01 +0700

    v1.6-1

--- src/app/leaderboard/[name]/page.tsx
+++ src/app/leaderboard/[name]/page.tsx
@@ -2,15 +2,22 @@
 
 import useSWR from "swr";
 import axios from "axios";
+import { usePathname } from "next/navigation";
 
 const fetcher = (url: any) => axios.get(url).then((res) => res.data);
 
 const Page = () => {
-  const { data } = useSWR("https://api.reforged.world/v1/guilds", fetcher);
+  const pathname = usePathname();
+  const pathArray = pathname.split("/").filter(Boolean);
+
+  const { data } = useSWR(
+    `https://api.reforged.world/v1/guild/${pathArray[1]}`,
+    fetcher,
+  );
 
   return (
     <div>
-      <div className="container mx-auto">Search Bar</div>
+      <div className="container mx-auto">{pathArray[1]}</div>
       <br />
       <div className="container mx-auto">{JSON.stringify(data)}</div>
     </div>
